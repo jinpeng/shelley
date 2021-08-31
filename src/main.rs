b0b94e05247cbe8b3e3a4ca7779724ef4286af93
jinpeng <jinpeng.dong@gmail.com>
2021-08-31 23:20:46 +0800

    sqlx mysql tokio query to struct

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,49 @@
 use std::env;
-use chrono::{DateTime, Utc};
+use chrono::{NaiveDateTime, NaiveDate};
 use serde::{Serialize, Deserialize};
 use sqlx::mysql::MySqlPool;
 
 #[derive(Serialize, Deserialize, Debug, Clone, sqlx::FromRow)]
 pub struct Poem {
-    pub id: uuid::Uuid,
+    pub id: String,
     pub author: String,
     pub title: String,
-    pub poem_type: i32,
-    pub dynasty: i32,
+    pub poem_type: i8,
+    pub dynasty: i8,
     pub paragraphs: String,
-    pub create_time: DateTime<Utc>,
-    pub update_time: DateTime<Utc>,
+    pub create_time: NaiveDateTime,
+    pub update_time: NaiveDateTime,
+}
+
+// Not neccessory
+impl Default for Poem {
+    fn default() -> Self {
+        Poem {
+            id: "".to_string(),
+            author: "".to_string(),
+            title: "".to_string(),
+            poem_type: 100,
+            dynasty: 100,
+            paragraphs: "".to_string(),
+            create_time: NaiveDate::from_ymd(1970, 1, 1).and_hms(0, 0, 0),
+            update_time: NaiveDate::from_ymd(1970, 1, 1).and_hms(0, 0, 0),
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
     let pool = MySqlPool::connect(&env::var("DATABASE_URL")?).await?;
-    list_poems(&pool).await?;
+    let poems = list_poems(&pool).await?;
+    println!("{:#?}", poems);
     Ok(())
 }
 
-async fn list_poems(pool: &MySqlPool) -> anyhow::Result<()> {
-    let recs = sqlx::query!(
+async fn list_poems(pool: &MySqlPool) -> Result<Vec<Poem>, sqlx::Error> {
+    let recs = sqlx::query_as!(
+        Poem,
         r#"
-SELECT id, author, title
+SELECT id, author, title, type as poem_type, dynasty, paragraphs, create_time, update_time
 FROM poem
 ORDER BY id
 limit 10
@@ -37,14 +54,14 @@ limit 10
 
     // NOTE: Booleans in MySQL are stored as `TINYINT(1)` / `i8`
     //       0 = false, non-0 = true
-    for rec in recs {
-        println!(
-            "- {} by {}: {}",
-            &rec.id,
-            &rec.author,
-            &rec.title,
-        );
-    }
+    // for rec in recs {
+    //     println!(
+    //         "- {} by {}: {}",
+    //         &rec.id,
+    //         &rec.author,
+    //         &rec.title,
+    //     );
+    // }
 
-    Ok(())
+    Ok(recs)
 }
